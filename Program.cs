d5878555d07cf8ec664420280fecb21c8b30b30c
ActiveGamers <162814473+ActiveGamers@users.noreply.github.com>
2025-03-04 18:05:44 +0330

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ali_Clock
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ali Joooon Saat Morede Nazaret Ro Vared Bokon:");
+            int ClockAli = Convert.ToInt32(Console.ReadLine());
+
+            if (ClockAli >= 0 && ClockAli <= 6)
+            {
+                Console.WriteLine("Nime Shab Be Kheir");
+            }
+            else if (ClockAli >= 6 && ClockAli <= 12)
+            {
+                Console.WriteLine("Sobh Be Kheir");
+            }
+            else if (ClockAli >= 12 && ClockAli <= 18)
+            {
+                Console.WriteLine("Bade Zohr Be Kheir");
+            }
+            else if (ClockAli >= 18 && ClockAli <= 24)
+            {
+                Console.WriteLine("Shab Be Kheir, Boro Begir Bekhab");
+            }
+            else
+            {
+                Console.WriteLine("Ali Jan, Saat Dorost Ro Vared Benamie");
+            }
+
+        }
+    }
+}
